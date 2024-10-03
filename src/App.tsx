2d849a717662bd82877e1ed2f94f3792601fdd1e
Nikolay Litvinov <nlit3926@gmail.com>
2024-10-03 13:48:29 +0200

    создал Rating, Accordion и Star

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
 import './App.css';
+import {findAllByDisplayValue} from "@testing-library/react";
 
 function App() {
-  return (
-    <div >
-     Hello
-    </div>
+  return (<div>
+          <Rating/>
+          <Accordion/>
+  </div>
+
   );
+}
+
+const Rating = () =>  {
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    )
+}
+
+const Accordion = () => {
+    return (
+        <div>
+            <h3>Меню</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
+}
+
+const Star = () => {
+    return <div>star</div>
 }
 
 export default App;
