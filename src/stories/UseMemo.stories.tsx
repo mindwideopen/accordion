e2b3194514499a2f87881fe94583cbad15cfa3d7
Nikolay Litvinov <nlit3926@gmail.com>
2024-12-02 17:41:52 +0200

    закончил useCallback()

--- src/stories/UseMemo.stories.tsx
+++ src/stories/UseMemo.stories.tsx
@@ -1,92 +1,148 @@
-import React, {useMemo, useState} from 'react';
+import React, {useCallback, useMemo, useState} from 'react';
 
 export default {
     title: 'useMemo'
 }
 
-export const DifficultCounting= () => {
-    const [a, setA] = React.useState(3)
-    const [b, setB] = React.useState(3)
-    let resultA = 1
-    let resultB = 1
+// export const DifficultCounting= () => {
+//     const [a, setA] = React.useState(3)
+//     const [b, setB] = React.useState(3)
+//     let resultA = 1
+//     let resultB = 1
+//
+//     resultA = useMemo(() => {          // запомни tempResultA и не пересчитывай, пока не изменится [a]
+//         let tempResultA = 1
+//         for (let i = 1; i <= a; i++) {
+//             let fake = 0
+//             while (fake < 30000000) {
+//                 fake++
+//                 const fakeValue = Math.random()
+//             }
+//             tempResultA = tempResultA * i
+//         }
+//         return tempResultA
+//     }, [a])
+//
+//
+//     for (let i = 1; i <= b; i++) {
+//         resultB = resultB * i
+//     }
+//
+//     return <>
+//         <input type="text" value={a} onChange={(e) => setA(+e.currentTarget.value)}/>
+//         <input type="text" value={b} onChange={(e) => setB(Number(e.currentTarget.value))}/>
+//         <hr/>
+//
+//         <div>
+//             Result for A:{resultA}
+//         </div>
+//
+//         <div>
+//             Result for B:{resultB}
+//         </div>
+//     </>
+// }
+//
+// const UsersSecret = (props: UsersProsType) => {
+//     console.log('USERS SECRET')
+//
+//     return <div>
+//         {props.users.map((u, i) => <div key={i}>{u}</div>)}
+//     </div>
+// }
+//
+// const Users = React.memo(UsersSecret)
+//
+//
+// export const HelpsReactMemo = () => {
+//     console.log('HelpsReactMemo')
+//     const[counter, setCounter] = useState(0);
+//     const[users, setUsers] = useState(['Dima', 'Kolya']);
+//     const addUser = () => {
+//         setUsers([...users, 'Dani' ])
+//     }
+//     console.log(users)
+//
+//
+//     let newArray = useMemo(() => {
+//
+//         return users.filter(i => i.indexOf('i')>-1)
+//
+//     }, [users])
+//
+//
+//
+//     return <>
+//         <button onClick={() => setCounter(counter+1)}>+</button>
+//         <button onClick={addUser}>addUser</button>
+//
+//         <NewMessagesCounter count={counter}/>
+//         <Users users={newArray}/>
+//
+//     </>
+// }
 
-    resultA = useMemo(() => {          // запомни tempResultA и не пересчитывай, пока не изменится [a]
-        let tempResultA = 1
-        for (let i = 1; i <= a; i++) {
-            let fake = 0
-            while (fake < 30000000) {
-                fake++
-                const fakeValue = Math.random()
-            }
-            tempResultA = tempResultA * i
-        }
-        return tempResultA
-    }, [a])
 
-
-    for (let i = 1; i <= b; i++) {
-        resultB = resultB * i
-    }
-
-    return <>
-        <input type="text" value={a} onChange={(e) => setA(+e.currentTarget.value)}/>
-        <input type="text" value={b} onChange={(e) => setB(Number(e.currentTarget.value))}/>
-        <hr/>
-
-        <div>
-            Result for A:{resultA}
-        </div>
-
-        <div>
-            Result for B:{resultB}
-        </div>
-    </>
+type BooksProsType = {
+    books: string[],
+    addBook: () => void,
 }
 
-const UsersSecret = (props: UsersProsType) => {
-    console.log('USERS SECRET')
-
-    return <div>
-        {props.users.map((u, i) => <div key={i}>{u}</div>)}
-    </div>
-}
-
-const Users = React.memo(UsersSecret)
 
 
-export const HelpsReactMemo = () => {
-    console.log('HelpsReactMemo')
+export const LikeUseCallback = () => {
+
+
+    console.log('LikeUseCallback render')
+
+
     const[counter, setCounter] = useState(0);
-    const[users, setUsers] = useState(['Dima', 'Kolya']);
-    const addUser = () => {
-        setUsers([...users, 'Dani' ])
+    const[books, setBooks] = useState(['React', 'HTML', 'CSS', 'JavaScript', 'NodeJS']);
+
+
+
+
+
+    const addBook = () => {
+        setBooks([...books, 'Angular' ])
     }
-    console.log(users)
 
 
-    let newArray = useMemo(() => {
 
-        return users.filter(i => i.indexOf('i')>-1)
+    const memoizedAddBook = useCallback(() =>   addBook, [books])
 
-    }, [users])
+
+
+
+
 
 
 
     return <>
         <button onClick={() => setCounter(counter+1)}>+</button>
-        <button onClick={addUser}>addUser</button>
+        <Books  addBook={memoizedAddBook}  books={books}/>
+        <MessagesCounter count={counter}/>
 
-        <NewMessagesCounter count={counter}/>
-        <Users users={newArray}/>
 
     </>
 }
+const BooksSecret = (props: BooksProsType) => {
+    console.log('Books render')
+    return <div>
+        <button onClick={props.addBook}>addBook</button>
+        {props.books.map((book, i) => <div key={i}>{book}</div>)}
 
+    </div>
+}
+
+const Books = React.memo(BooksSecret)
 
 const NewMessagesCounter = (props: any) => {
+    console.log('Counter render')
     return <div>{props.count}</div>
 }
 
-type UsersProsType = {
-    users: Array<string>
-}
+const MessagesCounter = React.memo(NewMessagesCounter)
+
+
+
