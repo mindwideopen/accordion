36493fa6b4706926e6e943bdd2a9f5d10dd3b352
Nikolay Litvinov <nlit3926@gmail.com>
2024-12-17 17:09:36 +0200

    еще немного про useEffect

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -4,32 +4,61 @@ export default {
     title: 'useEffect'
 }
 
-export const UseEffectStories = () => {
+// export const UseEffectStories = () => {
+//
+//     const [counter, setCounter] = React.useState(1);
+//     const [fake, setFake] = React.useState(1);
+//     console.log('Сама компонента')
+//     useEffect(() => {
+//
+//         console.log('useEffect no array');
+//         document.title = counter.toString()
+//     })
+//     useEffect(() => {
+//
+//         console.log('useEffect empty array ');
+//         document.title = counter.toString()
+//     }, [])
+//     useEffect(() => {
+//
+//         console.log('useEffect counter');
+//         document.title = counter.toString()
+//     }, [counter])
+//     return (
+//         <div>
+//             hello, {counter}
+//             <button onClick={() => setCounter(1)}>counter</button>
+//             <button onClick={() => setFake(fake + 1)}>fake</button>
+//         </div>
+//     );
+// };
+export const SetTimeoutExample = () => {
 
     const [counter, setCounter] = React.useState(1);
-    const [counter2, setCounter2] = React.useState(1);
-    console.log('Сама компонента')
-    useEffect(() => {
+    const [fake, setFake] = React.useState(1);
 
-        console.log('useEffect no array');
-        document.title = counter.toString()
-    })
-    useEffect(() => {
+    console.log(' SetTimeoutExample  ' + counter)
 
-        console.log('useEffect empty array ');
-        document.title = counter.toString()
-    },[])
-    useEffect(() => {
+  useEffect (() => {
 
-        console.log('useEffect counter1');
-        document.title = counter.toString()
-    },[counter])
-    return (
-        <div>
-            hello, {counter}
-            <button onClick={() => setCounter(1)}>counter1</button>
-            <button onClick={() => setCounter2(counter2 + 1)}>counter2</button>
-        </div>
-    );
-};
+      setInterval (() => {
+          console.log('counter ' +  counter)
+          setCounter(state => state + 1)  // это вариант логики изменнеия стейта. counter зафиксирован в объекте useEffect де-то, но можно получить в state актуальное значение counter
+              //плюс вариант state => state + 1
+      }, 1000)
+      return () => {  //сюда описыватеся cleanUp функция
+
+
+      }
+  }, [])
+
+return (
+    <div>
+        hello, {counter}
+        <button onClick={() => setCounter(counter + 1)}>counter</button>
+        <button onClick={() => setFake(fake + 1)}>fake</button>
+    </div>
+);
+}
+;
 
